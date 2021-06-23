f1566d28c75e00e29b51eef094aa3928ce25adfa
mirbek <mirbek.25.52@gmail.com>
2021-06-23 19:05:40 +0600

    make first demo svg animation

---
+++ index.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector(".wrapper");
+const crack = document.querySelector(".crack");
+
+wrapper.addEventListener("click", ()=>{
+  crack.classList.toggle("crack-svg");
+}) 
